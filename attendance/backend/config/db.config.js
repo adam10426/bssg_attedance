5e747a7bcc105a3b7e6a49f0201f611a6341e33f
adam10426 <adamabid521@gmail.com>
2023-08-27 23:53:15 +0500

    added configuration for env

--- attendance/backend/config/db.config.js
+++ attendance/backend/config/db.config.js
@@ -1,10 +1,12 @@
 const {Client} = require('pg')
+require('dotenv').config()
+
 const db = new Client({
-  user: "bssg",
-  host: "localhost",
-  database: "attendance",
-  password: "admin",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 module.exports =db
