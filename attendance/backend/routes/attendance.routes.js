9cad87889a4ae02ae814819320a07c183f0e7bdb
adam10426 <adamabid521@gmail.com>
2023-09-28 01:23:31 +0500

    developed updateAttendnace, get member and miqat attendances apis

--- attendance/backend/routes/attendance.routes.js
+++ attendance/backend/routes/attendance.routes.js
@@ -1,8 +1,10 @@
 const router  = require('express').Router()
 const attendanceController = require('../controllers/attendance.controller')
 
-
+router.get('/miqat/:miqatId',attendanceController.getMiqatAttendance)
+router.get('/member/:memberId',attendanceController.getMemberAttendance)
 router.post('/mark/', attendanceController.markMemberAttendance)
+router.patch('/update/', attendanceController.updateMemberAttendance)
 
 
 module.exports = router
